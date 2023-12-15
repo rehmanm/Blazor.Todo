56130d14c1e797d4331ea1da3271215deb10a457
mrehman1 <muhammadrehman@rocketinnovationstudio.ca>
2023-12-15 16:31:16 -0500

    Added Todo Entitiy

---
+++ Blazor.Todo.Shared/Entities/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor.Todo.Shared.Entities
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
